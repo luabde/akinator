856547fcf74168bfff7c1efe42618195e2a20ac9
luabde <luciaabellan6@gmail.com>
2025-10-30 17:16:41 +0100

    Starting session file

--- includes/session.php
+++ includes/session.php
@@ -11,7 +11,26 @@
         session_start();
     }
 
-    
+    // Función en la que se inicializan las variables que se usaran en todo el proyetco
+    function inicializar_juego(){
+        if(!isset($_SESSION['preguntas_respondidas'])){
+            $_SESSION['preguntas_respondidas'] = [];
+        }
+
+        // Se guardan los personajes adivinados
+        if(!isset(($_SESSION['historial']))){
+            $_SESSION['historial'] = [];
+        }
+    }
+
+    // Función para guardar la respuesta y la columna en el array preguntas_respondidas
+    function guardarRespuesta($idPregunta, $columna, $respuestaUser){
+        // Se guarda en preguntas respondidas (array associativas) la columna y la respuesta del usuario
+        $_SESSION['preguntas_respondidas'][$idPregunta] = [
+            'columna' => $columna,
+            'respuesta' => $respuestaUser
+        ];
+    }
 
 
 ?>

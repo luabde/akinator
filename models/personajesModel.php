713db1e4c46a0e4310ddc97336f665e83d3997db
luabde <luciaabellan6@gmail.com>
2025-11-13 10:30:26 +0100

    Foto akinator provisional

--- models/personajesModel.php
+++ models/personajesModel.php
@@ -24,19 +24,19 @@
         
     }
 
-    $personajes = new Personaje();
-    $lista = $personajes->obtenerPersonajes();
-    // var_dump($lista);
-    foreach($lista as $personaje){
-         echo "
-            <div class='card'>
-                <div class='card-inner'>
-                    <img src='./{$personaje['imagen_url']}' alt='{$personaje['nombre']}' class='foto'>  
-                    <h3>{$personaje['nombre']}</h3>
-                    <p>{$personaje['descripcion']}</p>
-                </div>
-            </div>";
-    }
+    // $personajes = new Personaje();
+    // $lista = $personajes->obtenerPersonajes();
+    // // var_dump($lista);
+    // foreach($lista as $personaje){
+    //      echo "
+    //         <div class='card'>
+    //             <div class='card-inner'>
+    //                 <img src='./{$personaje['imagen_url']}' alt='{$personaje['nombre']}' class='foto'>  
+    //                 <h3>{$personaje['nombre']}</h3>
+    //                 <p>{$personaje['descripcion']}</p>
+    //             </div>
+    //         </div>";
+    // }
 
     
 // class PersonajesModel{

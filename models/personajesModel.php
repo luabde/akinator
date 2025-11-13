dc6f969bd59fe936b7920d490234c48f72ff0d41
luabde <luciaabellan6@gmail.com>
2025-11-13 10:22:06 +0100

    v1 personajes (de momento no es funcional del todo. Falta modular para que se muestre desde el index)

--- models/personajesModel.php
+++ models/personajesModel.php
@@ -1,6 +1,43 @@
 <?php
     require '../config/database.php';
-    conectarDB();
+    
+    class Personaje{
+        private $arrayPersonajes;
+        private $db;
+
+        // Definimos el constructor
+        public function __construct(){
+            // Se tiene que crear una nueva instancia de la base de datos.
+            $this->db = conectarDB();
+        }
+
+        // Definimos los metodos para los personajes
+        public function obtenerPersonajes(){
+            $query = 'SELECT * FROM personajes';
+            $resultado = mysqli_query($this->db, $query);
+
+            // Convertimos el resultado en un array
+            $this->arrayPersonajes = mysqli_fetch_all($resultado, MYSQLI_ASSOC);
+
+            return $this->arrayPersonajes;
+        }
+        
+    }
+
+    $personajes = new Personaje();
+    $lista = $personajes->obtenerPersonajes();
+    // var_dump($lista);
+    foreach($lista as $personaje){
+         echo "
+            <div class='card'>
+                <div class='card-inner'>
+                    <img src='./{$personaje['imagen_url']}' alt='{$personaje['nombre']}' class='foto'>  
+                    <h3>{$personaje['nombre']}</h3>
+                    <p>{$personaje['descripcion']}</p>
+                </div>
+            </div>";
+    }
+
     
 // class PersonajesModel{
 //     private $db;

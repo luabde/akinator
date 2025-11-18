d593ed2e32539409d32f10c12b17b588519df763
pau <pausoldevilla@gmail.com>
2025-11-18 08:33:23 +0100

    Vista de no respuesta aki

--- views/sin_resultados.php
+++ views/sin_resultados.php
@@ -1,4 +1,11 @@
-<p>SIN RESULTADOS</p>
-<div class="botones">
-    <a href="../public/index.php?seccio=nueva" class="btn-primary-si">Vover a jugar</a>
+<div class="resultado">
+  <p>¡Ups! No he logrado identificar a tu personaje.</p>
+
+  <img src="img/interrogante.png" alt="Interrogante" class="foto-adivinar">
+
+  <p>Tu elección no coincide con ninguno de los personajes que conozco.</p>
+
+  <div class="botones">
+    <a href="../public/index.php?seccio=nueva" class="btn-primary-si">Intentar de nuevo</a>
+  </div>
 </div>

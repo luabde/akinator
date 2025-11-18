c0f48ea7e249d2600d631c0b5099009d8e4319c4
luabde <luciaabellan6@gmail.com>
2025-11-18 11:56:16 +0100

    no m acuerdo que habia cambiado:)

--- public/index.php
+++ public/index.php
@@ -24,21 +24,8 @@ if (isset($_GET['seccio']) && $_GET['seccio'] === 'nueva') {
 }
 
 // Inicialización segura (solo si no existen)
-$_SESSION['historial'] = $_SESSION['historial'] ?? [];
 $_SESSION['vista'] = $_SESSION['vista'] ?? 'inicio';
 
-// -------------------
-// FUNCIONES
-// -------------------
-// function mostraHistorial() {
-//     if (!empty($_SESSION['historial'])) {
-//         echo "<ul class='llista-sidebar'>";
-//         foreach (array_reverse($_SESSION['historial']) as $h) echo "<li>$h</li>";
-//         echo "</ul>";
-//     } else {
-//         echo "<h2 class='historial-buit'>No hi ha encerts</h2>";
-//     }
-// }
 $seccio = $_GET['seccio'] ?? '';
 ?>
 
